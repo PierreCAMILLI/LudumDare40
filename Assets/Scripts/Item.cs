0c1f2b70e4bac561754f23a016d0b9753c92efb6
Thibault <thibault.laine@inria.fr>
2017-12-02 19:57:47 +0100

    inventory finished

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -39,6 +39,9 @@ public class Item : MonoBehaviour
     public Element element;
     public bool thrown = false;
     public bool cooldownSensitive = false;
+    public float cooldownTime;
+
+    private Rigidbody2D rb = null;
 
 	// Use this for initialization
 	void Start ()
@@ -49,6 +52,17 @@ public class Item : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if(thrown)
+        {
+            if (!rb) rb = GetComponent<Rigidbody2D>();
+            if (rb.velocity.magnitude <= 0.001F) thrown = false;
+        }
+
+        if(cooldownSensitive)
+        {
+            cooldownTime -= Time.deltaTime;
+            if (cooldownTime <= 0.0F)
+                Destroy(this.gameObject);
+        }
 	}
 }
