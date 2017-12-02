9c28150f9708fde16e0e68cb4f5e3e1c30b0911b
Pierre CAMILLI <camillipierre@gmail.com>
2017-12-02 11:52:56 +0100

    Add packages and first scene

---
+++ Assets/Scripts/Controls/Controls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : SingletonBehaviour<Controls> {
+
+    [SerializeField]
+    private PlayerControls[] _players;
+
+}
+
+[System.Serializable]
+public class PlayerControls
+{
+
+}
